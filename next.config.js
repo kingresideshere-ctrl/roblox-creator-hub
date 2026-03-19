a840407e2a6c2b7bd0e3029376d8ec270908b561
kingresideshere-ctrl <kingresideshere@gmail.com>
2026-03-19 18:39:35 -0400

    Allow all image domains for profile pictures

--- next.config.js
+++ next.config.js
@@ -2,13 +2,7 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'yt3.ggpht.com' },
-      { protocol: 'https', hostname: 'yt3.googleusercontent.com' },
-      { protocol: 'https', hostname: '**.cdninstagram.com' },
-      { protocol: 'https', hostname: 'p16-sign-sg.tiktokcdn.com' },
-      { protocol: 'https', hostname: '**.tiktokcdn.com' },
-      { protocol: 'https', hostname: 'unavatar.io' },
-      { protocol: 'https', hostname: '**.googleusercontent.com' },
+      { protocol: 'https', hostname: '**' },
     ],
   },
 };
